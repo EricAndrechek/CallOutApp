3b288d62d944976502547b43c5e099391abf76ea
Eric Andrechek <ericandrechek@gmail.com>
2019-01-18 23:34:27 -0500

    updated python routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def landing():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    if request.method == "GET":
+        return redirect('login')
+    elif request.method == "POST":
+        return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
