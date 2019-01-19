a73ab9b294ff9bebb3c8f86224d6842df0fe7792
Eric Andrechek <ericandrechek@gmail.com>
2019-01-19 00:34:31 -0500

    setup login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
@@ -10,7 +10,18 @@ def landing():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        phone = request.form.get('phone')
+        password = request.form.get('password')
+        # check for phone in database and it it exists check if password matches
+        # account = dbc(phone, password)
+        account = False
+        if account:  # if account exists is True, else False
+            return redirect("dashboard")
+        else:
+            return render_template("login.html", error="Incorrect username or password<br>")
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
